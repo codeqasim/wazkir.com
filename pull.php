eebba0a4f84aa6345c35157f97f4cc5857e81bd1
Qasim Hussain <44278286+qaxim@users.noreply.github.com>
2020-05-01 17:46:17 -0700

    Create pull.php

---
+++ pull.php
@@ -0,0 +1,7 @@
+<?php
+
+shell_exec( 'git pull origin master' );
+echo "<style>h3{text-align: center; font-family: sans-serif; background: #eee; padding: 50px; border-bottom: 2px solid #d5d5d5;}></style>";
+echo "<h3>GIT PULLED</h3>";
+
+?>
